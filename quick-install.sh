ef12e020ab9c50697a0728ebaaf1ce0d0c00fc0f
Inactive Virus <gigagerm@hotmail.com>
2020-04-03 00:12:56 +0800

    source after install

--- quick-install.sh
+++ quick-install.sh
@@ -2,4 +2,5 @@
 BASEPATH=$(cd `dirname $0`; pwd)
 cp $BASEPATH/nginx/ng* /usr/local/bin
 cp $BASEPATH/nginx_complete/nginx /etc/bash_completion.d/
+source  /etc/bash_completion.d/nginx
 echo 'Scripts copied to destination.' 
